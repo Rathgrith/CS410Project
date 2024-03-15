309c6601ab4c9d250cbd70a581b7ddfffdcaff07
Eunice Chan <eunice.ch@live.com>
2024-03-15 02:49:03 -0500

    BE and FE scaffolding

---
+++ ui/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
